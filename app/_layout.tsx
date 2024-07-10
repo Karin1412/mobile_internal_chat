15dd213b4c7a19a7af2e7bfd8f50180fb9c50b10
Karin1412 <100667390+Karin1412@users.noreply.github.com>
2024-07-10 08:57:07 +0700

    commit

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,37 +1,52 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import { Ionicons } from '@expo/vector-icons';
+import { ConvexProvider, ConvexReactClient } from 'convex/react';
+import { Link, Stack } from 'expo-router';
+import { TouchableOpacity } from 'react-native';
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
+  unsavedChangesWarning: false,
+});
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
-
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
+// Stack navigation with two screens and one modal
+export default function RootLayoutNav() {
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
+    <ConvexProvider client={convex}>
+      <Stack
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: '#EEA217',
+          },
+          headerTintColor: '#fff',
+        }}>
+        <Stack.Screen
+          name="index"
+          options={{
+            headerTitle: 'My Chats',
+            headerRight: () => (
+              <Link href={'/(modal)/create'} asChild>
+                <TouchableOpacity>
+                  <Ionicons name="add" size={32} color="white" />
+                </TouchableOpacity>
+              </Link>
+            ),
+          }}
+        />
+        <Stack.Screen name="(chat)/[chatid]" options={{ headerTitle: 'Test' }} />
+        <Stack.Screen
+          name="(modal)/create"
+          options={{
+            headerTitle: 'Start a Chat',
+            presentation: 'modal',
+            headerLeft: () => (
+              <Link href={'/'} asChild>
+                <TouchableOpacity>
+                  <Ionicons name="close-outline" size={32} color="white" />
+                </TouchableOpacity>
+              </Link>
+            ),
+          }}
+        />
       </Stack>
-    </ThemeProvider>
+    </ConvexProvider>
   );
 }
